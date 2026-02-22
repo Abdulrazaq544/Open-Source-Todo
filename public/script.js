3141f0aec7aebbc89e2fde5fc9d40141ac22061b
Abdulrazaq544 <Garbaabdu31@gmail.com>
2026-02-22 13:56:34 -0800

    Add error handling for task input validation
    Fix error handling for task input and display error message.

--- public/script.js
+++ public/script.js
@@ -9,4 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("App Initialized");
     // Load tasks will be called here later (Issue #9)
 });
-
+ addBtn.addEventListener('click', () =>
+        {
+        const text = taskInput.value.trim();
+        const errorMsg = document.getElementNyId('error-msg');
+        if (text === ") {
+        errorMsg.style.display = 'block';
+        return;
+        }
+        errorMsg.style.display = 'none';addTask(text); 
+        taskInput.value = ";
+        });
